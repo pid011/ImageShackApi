642790a44e05c061c5e3a66402d210febd94e21b
Sepi <deet1022@gmail.com>
2017-03-11 02:08:27 +0900

    Add Tools class, JsonParse method

--- ImageShackApi/ImageShackApi/ImageShackUploader.cs
+++ ImageShackApi/ImageShackApi/ImageShackUploader.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -24,9 +25,42 @@ namespace ImageShackApi
                 throw new FileNotFoundException($"file not found: {filePath}");
             }
 
-            var result = new UploadResult();
+            // return JsonParse();
+        }
 
-            return result;
+        private UploadResult JsonParse(Stream imageInfoJson)
+        {
+            string json = null;
+            using (var reader = new StreamReader(imageInfoJson))
+            {
+                json = reader.ReadToEnd();
+            }
+
+            if (json == null)
+            {
+                throw new NullReferenceException();
+            }
+            JObject o = JObject.Parse(json);
+
+            return new UploadResult()
+            {
+                Rating = new UploadResult.RatingDetails()
+                {
+                    Ratings = int.Parse((string)o["rating"]["ratings"]),
+                    Average = double.Parse((string)o["rating"]["avg"])
+                },
+                Links = new UploadResult.LinkDetails()
+                {
+                    ImageLink = (string)o["links"]["image_link"],
+                    ThumbLink = (string)o["links"]["thumb_link"]
+                },
+                Resolution = new UploadResult.ResolutionDetails()
+                {
+                    Width = int.Parse((string)o["resolution"]["width"]),
+                    Height = int.Parse((string)o["resolution"]["height"])
+                }
+            };
+            
         }
     }
 }
---
+++ ImageShackApi/ImageShackApi/Tools.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageShackApi
+{
+    public class Tools
+    {
+        public static string GetContentType(FileInfo info)
+        {
+            switch (info.Extension.ToLower())
+            {
+                case ".jpg":
+                    return "image/jpg";
+
+                case ".jpeg":
+                    return "image/jpeg";
+
+                case ".gif":
+                    return "image/gif";
+
+                case ".png":
+                    return "image/png";
+
+                case ".bmp":
+                    return "image/bmp";
+
+                case ".tif":
+                    return "image/tiff";
+
+                case ".tiff":
+                    return "image/tiff";
+
+                default:
+                    return "image/unknown";
+            }
+        }
+    }
+}
