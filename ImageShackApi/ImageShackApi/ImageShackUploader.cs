92b591f1089127747629c760de65051e062e497b
Sepi <deet1022@gmail.com>
2017-03-11 01:47:25 +0900

    Add ImageShackUploader class

---
+++ ImageShackApi/ImageShackApi/ImageShackUploader.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageShackApi
+{
+    public class ImageShackUploader
+    {
+        public static string ApiKey { get; set; }
+
+        public static string UploadImage(string filePath)
+        {
+            if (string.IsNullOrWhiteSpace(ApiKey))
+            {
+                throw new InvalidOperationException("The API Key is empty.");
+            }
+
+            var fileInfo = new FileInfo(filePath);
+            if (!fileInfo.Exists)
+            {
+                throw new FileNotFoundException($"file not found: {filePath}");
+            }
+        }
+    }
+}
